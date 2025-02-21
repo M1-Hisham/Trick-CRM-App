50954efe232034d274bf5866e759e61707e6a705
Mohamed Hisham <mohamedhisham67751@gmail.com>
2025-02-21 16:39:38 +0200

    Add Open Activities feature: Implement OpenActivitiesScreen and card view for activities

--- lib/features/leads/lead-view/Open-Activities/presentation/open_activities_screen.dart
+++ lib/features/leads/lead-view/Open-Activities/presentation/open_activities_screen.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:trick_crm_app/core/widgets/app_bar.dart';
+
+import 'widget/card_lead_view.dart';
 
 class OpenActivitiesScreen extends StatelessWidget {
   const OpenActivitiesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: appBar('Open Activities'),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: Column(
+          children: [
+            cardActivitesView(
+              title: 'Open Tasks',
+              icon: 'OpenTasks',
+              onTap: () {
+                // Get.to(() => OpenTasksScreen());
+              },
+            ),
+            cardActivitesView(
+              title: 'Open Meetings',
+              icon: 'OpenMeetings',
+              onTap: () {
+                // Get.to(() => OpenMeetingsScreen());
+              },
+            ),
+            cardActivitesView(
+              title: 'Open Calls',
+              icon: 'OpenCalls',
+              onTap: () {
+                // Get.to(() => OpenCallsScreen());
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

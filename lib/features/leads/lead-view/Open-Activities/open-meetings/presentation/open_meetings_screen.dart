f4a3b2913813cb277f150954546c3af1e36bcaff
Mohamed Hisham <mohamedhisham67751@gmail.com>
2025-03-17 09:42:29 +0200

    Add MeetingModel and integrate it into OpenActivitiesScreen and OpenMeetingsScreen

--- lib/features/leads/lead-view/Open-Activities/open-meetings/presentation/open_meetings_screen.dart
+++ lib/features/leads/lead-view/Open-Activities/open-meetings/presentation/open_meetings_screen.dart
@@ -2,33 +2,68 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:trick_crm_app/core/helpers/spacing.dart';
 import 'package:trick_crm_app/core/widgets/app_button.dart';
+import 'package:trick_crm_app/features/leads/lead-view/Open-Activities/open-meetings/data/model/meeting_model.dart';
 
 import '../../../../../../core/resources/resources.dart';
 import '../../../../../../core/widgets/app_bar.dart';
+import '../../../../../../core/widgets/app_data_table.dart';
 
 class OpenMeetingsScreen extends StatelessWidget {
-  const OpenMeetingsScreen({super.key});
+  final List<MeetingModel> meetings;
+  const OpenMeetingsScreen({super.key, required this.meetings});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: appBar('Open Meetings'),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            AppButton(
-              icon: SvgPicture.asset(R.icons.add),
-              text: 'Create a New Meeting',
-              onPressed: () {},
-            ),
-            spacingV(20),
-            Text(
-              'Open Meetings',
-              style: R.textStyles.font17PrimaryW600,
-            ),
-          ],
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: appBar('Open Meetings'),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              AppButton(
+                icon: SvgPicture.asset(R.icons.add),
+                text: 'Create a New Meeting',
+                onPressed: () {},
+              ),
+              spacingV(20),
+              Text(
+                'Open Meetings',
+                style: R.textStyles.font17PrimaryW600,
+              ),
+              spacingV(20),
+              AppDataTable<MeetingModel>(
+                dataMessage: 'No Open Meetings',
+                data: meetings,
+                headers: const [
+                  'Subject',
+                  'From',
+                  'To',
+                  'Related To',
+                  // 'Lead Name',
+                  // 'Contact Name',
+                  // 'Client Name',
+                  'Host',
+                  'Status',
+                ],
+                dataExtractors: [
+                  (meetings) => meetings.title ?? '_',
+                  (meetings) => meetings.startTime ?? '_',
+                  (meetings) => meetings.endTime ?? '_',
+                  (meetings) => meetings.relatedTo ?? '_',
+                  // (meetings) => meetings.
+                  // (meetings) => meetings.contactName ?? '_',
+                  // (meetings) => meetings.clientName ?? '_',
+                  (meetings) => meetings.host?.name ?? '_',
+                  (meetings) => meetings.status ?? '_',
+                ],
+                dataIdExtractor: (meetings) => (meetings.id ?? 0).toString(),
+                onViewDetails: (id, leadName) {},
+              ),
+            ],
+          ),
         ),
       ),
     );
